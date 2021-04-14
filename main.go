910923afa118f67ea4b00bf6ffb68b694f4d8170
ikanago <ikanago@ikanago.dev>
2021-04-14 21:08:27 +0900

    PivotRoot with relative path

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	// "io/ioutil"
 	"os"
 	"os/exec"
+	"path"
 	"syscall"
 )
 
@@ -24,7 +24,10 @@ func parent() {
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS,
+		// syscall.CLONE_NEWUSER,
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -36,30 +39,71 @@ func parent() {
 	}
 }
 
-func child() {
+func child() error {
 	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
 
-	try(syscall.Sethostname([]byte("container")))
-	try(syscall.Mount("proc", "/root/rootfs/proc", "proc", uintptr(syscall.MS_NOEXEC|syscall.MS_NOSUID|syscall.MS_NODEV), ""))
-	try(syscall.Chdir("/root"))
-	try(syscall.Mount("rootfs", "/root/rootfs", "", syscall.MS_BIND|syscall.MS_REC, ""))
-	try(os.MkdirAll("/root/rootfs/oldrootfs", 0700))
-	try(syscall.PivotRoot("rootfs", "/root/rootfs/oldrootfs"))
-	fmt.Println("reached")
-	try(syscall.Unmount("/oldrootfs", syscall.MNT_DETACH))
-	try(os.RemoveAll("/oldrootfs"))
-	try(os.Chdir("/"))
+	if err := syscall.Sethostname([]byte("container")); err != nil {
+		fmt.Printf("Cannot change hostname: %v\n", err)
+	}
+
+	pivotBase := "./root"
+	rootfs := "rootfs"
+	if err := syscall.Mount("proc", path.Join(pivotBase, rootfs, "proc"), "proc", uintptr(syscall.MS_NOEXEC|syscall.MS_NOSUID|syscall.MS_NODEV), ""); err != nil {
+		fmt.Printf("Cannot mount proc: %v\n", err)
+		return err
+	}
+
+	if err := pivotRoot(pivotBase, rootfs); err != nil {
+		return err
+	}
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	try(cmd.Run())
-
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Command execution failed: %v\n", err)
+		return err
+	}
+	return nil
 }
 
-func try(err error) {
-	if err != nil {
-		panic(err)
+func pivotRoot(pivotBase string, rootfs string) error {
+	if err := syscall.Chdir(pivotBase); err != nil {
+		fmt.Printf("Cannot change dir to %s: %v\n", pivotBase, err)
+		return err
 	}
+
+	// Cannot mount 'root/rootfs' from '../root'.
+	if err := syscall.Mount("rootfs", rootfs, "", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
+		fmt.Printf("Cannot mount ./root/rootfs: %v\n", err)
+		return err
+	}
+
+	oldrootfs := "oldrootfs"
+	if err := os.MkdirAll(path.Join(rootfs, oldrootfs), 0700); err != nil {
+		fmt.Printf("Cannot mkdir ./root/rootfs/oldrootfs: %v\n", err)
+		return err
+	}
+
+	if err := syscall.PivotRoot("rootfs", path.Join(rootfs, oldrootfs)); err != nil {
+		fmt.Printf("PivotRoot failed: %v\n", err)
+		return err
+	}
+
+	if err := os.Chdir("/"); err != nil {
+		fmt.Printf("Cannot change dir to /: %v\n", err)
+		return err
+	}
+
+	if err := syscall.Unmount(path.Join("/", oldrootfs), syscall.MNT_DETACH); err != nil {
+		fmt.Printf("Cannot unmount /oldrootfs: %v\n", err)
+		return err
+	}
+
+	if err := os.RemoveAll(path.Join("/", oldrootfs)); err != nil {
+		fmt.Printf("Cannot remove /oldrootfs: %v\n", err)
+		return err
+	}
+	return nil
 }
